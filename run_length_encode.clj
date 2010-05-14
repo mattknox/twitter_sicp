668f4567aa1ed59ba717f1062f3630225ab64920
matt knox <matthewknox@gmail.com>
2010-05-13 20:44:43 -0700

    made rle return one big list.

--- run_length_encode.clj
+++ run_length_encode.clj
@@ -1,5 +1,5 @@
 (defn run-length-encode [count [current & l]]
      (cond (nil? current) '()
-           (empty? l) (list (list count current))
+           (empty? l) (list count current)
            (= current (first l)) (run-length-encode (inc count) (rest l))
-           true (cons (list count current) (run-length-encode 1 l))))
+           true (cons count (cons current (run-length-encode 1 l)))))
