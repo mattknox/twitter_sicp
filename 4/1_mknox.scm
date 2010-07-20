ffa9d9b79c0082a40008e3ecf43246184350045e
matt knox <matthewknox@gmail.com>
2010-07-20 16:06:23 -0700

    added a comment so I can test git push with new SSH key

--- 4/1_mknox.scm
+++ 4/1_mknox.scm
@@ -5,14 +5,17 @@
 ;;       (cons (eval (first-operand exps) env)
 ;;             (list-of-values (rest-operands exps) env))))
 
+;; evals left-to-right
 (define (list-of-values-l-r exps env)
   (if (no-operands? exps)
       '()
       (let ((x (eval (first-operand exps) env)))
         (cons x (list-of-values (rest-operands exps) env)))))
 
+;; evals right-to-left
 (define (list-of-values-r-l exps env)
   (if (no-operands? exps)
       '()
       (let ((xs (list-of-values (rest-operands exps) env)))
         (cons (eval (first-operand exps) env) xs))))
+
