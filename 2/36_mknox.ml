580f3c8f64dd756c3a12530f766d11ceab9e6faf
matt knox <matthewknox@gmail.com>
2011-07-14 14:46:20 -0700

    made a better foldr

--- 2/36_mknox.ml
+++ 2/36_mknox.ml
@@ -1,3 +1,6 @@
 let rec foldr f v l =
-  match l with [] -> y
-    |   x::xs -> f x (foldr f xs y);;
+match l with [] -> v
+  |   x::xs -> f x (foldr f v xs);;
+
+let prod = foldr (function a -> function x -> a * x) 1;;
+(prod [1;2;3;4], prod [1;2;3;4;5]);;
